ade7e4dbad336402dcdd6e832cf1eaf89a035d3b
Kevin Nguyen <7575701@isd535.org>
2022-03-18 11:01:41 -0500

    datastoreadded

---
+++ scripts/datastore.js
@@ -0,0 +1,13 @@
+(function (window) {
+    'use strict' ;
+
+    
+let App = window.App || {};
+
+function DatStore() {
+    console. log('Running the DatStore Function...')
+}
+
+App.DatStore = DataStore;
+window.App = App;
+})(window);
