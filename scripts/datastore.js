0e9bd08fc432eddc06192345c19383f380afc77f
Kevin Nguyen <7575701@isd535.org>
2022-03-21 11:03:17 -0500

    added prototype

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -5,9 +5,12 @@
 let App = window.App || {};
 
 function DataStore() {
-    console. log('Running the DatStore Function...')
     this.data = {};
 }
+
+DataStore.prototype.add = function (key,val) {
+    this.data[key] = val;
+};
 
 App.DataStore = DataStore;
 window.App = App;
