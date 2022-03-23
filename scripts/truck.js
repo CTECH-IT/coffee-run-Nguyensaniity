d1b950bf71d8f42941e827aebbe41830107df67a
Kevin Nguyen <7575701@isd535.org>
2022-03-23 10:43:46 -0500

    created order

--- scripts/truck.js
+++ scripts/truck.js
@@ -4,8 +4,15 @@
     let App = window.App || {};
 
     function Truck (truckID, db) {
+        this.truckID = truckID;
+        this.db = db;
     }
 
+    Truck.prototype.createOrder = function (order) {
+        console.log('Adding order for' + order.emailAdress);
+        this.db.add(order.emailAdress, order);
+    }
+    
     App.Truck = Truck;
     window.App = App; 
 
