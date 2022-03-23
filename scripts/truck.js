fc3470e2a063fd4c8b4013313f1145f97f85c38f
Kevin Nguyen <7575701@isd535.org>
2022-03-23 10:39:39 -0500

    forgot truck

---
+++ scripts/truck.js
@@ -0,0 +1,12 @@
+(function (window) {
+    'use strict';
+
+    let App = window.App || {};
+
+    function Truck (truckID, db) {
+    }
+
+    App.Truck = Truck;
+    window.App = App; 
+
+})(window);
