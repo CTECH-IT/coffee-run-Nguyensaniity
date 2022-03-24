643ba6af0c6cc52b2f25058ac049002389923e69
Kevin Nguyen <7575701@isd535.org>
2022-03-24 11:13:31 -0500

    more mainjs

--- scripts/main.js
+++ scripts/main.js
@@ -4,5 +4,9 @@
     let app = window.App;
     let Truck = App.Truck
     let DataStore = App.DataStore;
+
+    let myTruck = new Truck('1234', new DataStore());
+
+    window.myTruck = myTruck;
     
 })(window);
