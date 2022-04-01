0bada4490b6317962aaad31a81a085d9eaf1632d
Kevin Nguyen <7575701@isd535.org>
2022-04-01 11:28:53 -0500

    added serialize array

--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -4,10 +4,27 @@
     let App = window.App || {};
     let $ = window.jQuery;
 
-    function FormHandler() {
+    function FormHandler(selector) {
         if (!selector) {
             throw new Error('No selector provided!');
         }
+
+
+        // find the "selector" in the DOM using jQuery and assign it  to this .formElement
+        this.$formElement = $(selector);
+        if (this.$formElement.length == 0) {
+            throw new Error('Could not find element with selector: ' + selector);
+        }
+    }
+
+    FormHandler.prototype.addSubmitHandler = function () {
+        console.log('Setting the submit handler for the form...');
+        this.$formElement.on('submit', function(event) {
+            event.preventDefault();
+
+            let data = $(this).serializeArray();
+            console.log(data);
+        });
     }
 
     App.FormHandler = FormHandler;
