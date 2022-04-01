f16c8f452aa1dc665cfaadac959817e15e638484
Kevin Nguyen <7575701@isd535.org>
2022-04-01 11:49:56 -0500

    ad

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,22 @@
 (function(window) {
     'use strict';
 
+    const FORM_SELECTOR = '[data-coffee-order="form"]'
+
+    //Let's make sure we only have on of these:
     let app = window.App;
     let Truck = App.Truck
     let DataStore = App.DataStore;
+    let FormHandler = App.FormHandler
 
     let myTruck = new Truck('1234', new DataStore());
 
     window.myTruck = myTruck;
-    
+
+    //find the form that is being submitted and create a formHander object
+    let formHandler = new FormHandler(FORM_SELECTOR);
+    //bind the createOrder to a specific truck and pass it to addSubmitHandler
+    formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
+    console.log(formHandler);
+
 })(window);
