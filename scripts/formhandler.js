53742493e61473a58e3a186ffb6bc30f703d827a
Kevin Nguyen <7575701@isd535.org>
2022-04-01 11:15:27 -0500

    formhandler

---
+++ scripts/formhandler.js
@@ -0,0 +1,16 @@
+(function (window) {
+    'use strict';
+
+    let App = window.App
+    let $ = window.jQuery;
+
+    function FormHandler() {
+        if (!selector) {
+            throw new Error('No selector provided!');
+        }
+    }
+
+    App.FormHandler = FormHandler;
+    window.App = App;
+
+})(window);
