d8e703dfb987f6959235f00ffc4886588d0e32c6
Kevin Nguyen <7575701@isd535.org>
2022-04-01 19:24:33 -0500

    truckj

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@
 
     const FORM_SELECTOR = '[data-coffee-order="form"]'
     const CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
+
     //Let's make sure we only have on of these:
     let app = window.App;
     let Truck = App.Truck
@@ -11,13 +12,15 @@
     let Checklist = App.Checklist;
 
     let myTruck = new Truck('1234', new DataStore());
-
+    let checkList = new Checklist(CHECKLIST_SELECTOR);
     window.myTruck = myTruck;
 
     //find the form that is being submitted and create a formHander object
     let formHandler = new FormHandler(FORM_SELECTOR);
-    //bind the createOrder to a specific truck and pass it to addSubmitHandler
-    formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
-    console.log(formHandler);
-
+    //when the submit button is called, create the order and add a checkbox
+    formHandler.addSubmitHandler(function (data) {
+        myTruck.createOrder.call(myTruck, data);
+        checkList.addRow.call(checkList, data);
+    });
+    
 })(window);
